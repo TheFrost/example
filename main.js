721421d01248b5736c4bd714b8d25fb1e5667460
ftinoco <ftinoco@ia.com.mx>
2016-08-02 16:10:05 -0500

    make it

---
+++ main.js
@@ -0,0 +1,25 @@
+// (function() {
+//   'use strinct';
+//
+//   var Module = Module || {
+//     create: function () {
+//       // var instance = Object.create(this);
+//       this.init();
+//       return Object.create(this);
+//     },
+//     init: function () {
+//       this.cache();
+//       this.bindEvents();
+//       return this;
+//     },
+//     cache: function () {
+//       console.log('cache');
+//     },
+//     bindEvents: function () {
+//       console.log('bind events');
+//     }
+//   };
+//
+//   window.Module = Module.init();
+//
+// }());
